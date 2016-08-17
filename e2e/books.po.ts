500b4edbd0b9bdc044c21188c30c65d0f3cd2d54
Robin Böhm <robin.boehm@symetics.com>
2016-08-17 06:20:36 +0200

    cleanup preworkshop

--- e2e/books.po.ts
+++
@@ -1,6 +0,0 @@
-import {browser, element, by, By, $, $$, ExpectedConditions} from 'protractor/globals';
-
-export class Books {
-  books = element.all(by.repeater('book in $ctrl.books'));
-  bookTitles = element.all(by.repeater('book in $ctrl.books').column('book.title'));
-}
--- protractor.conf.ts
+++ protractor.conf.ts
@@ -1,38 +1,21 @@
-
 import {Config} from 'protractor';
-let SpecReporter = require('jasmine-spec-reporter');
 
 
 export let config:Config = {
-  allScriptsTimeout : 11000,
-  specs             : [
-    'e2e/**/*.e2e.js'
-  ],
-  capabilities      : {
-    'browserName' : 'chrome'
-  },
-  directConnect     : true,
-  baseUrl           : 'http://localhost:3000/',
-  framework         : 'jasmine',
-  jasmineNodeOpts   : {
-    showColors             : true,
-    defaultTimeoutInterval : 30000,
-    print                  : function () {
+    allScriptsTimeout: 11000,
+    specs: [
+        'e2e/**/*.e2e.js'
+    ],
+    capabilities: {
+        'browserName': 'chrome'
+    },
+    directConnect: true,
+    baseUrl: 'http://localhost:3000/',
+    framework: 'jasmine',
+    jasmineNodeOpts: {
+        showColors: true,
+        defaultTimeoutInterval: 30000,
+        print: function () {
+        }
     }
-  },
-  onPrepare         : function () {
-    var reporters = require('jasmine-reporters');
-
-    jasmine.getEnv().addReporter(new reporters.JUnitXmlReporter({
-      savePath: 'xmloutput',
-      consolidateAll: true
-    }));
-
-    jasmine.getEnv().addReporter(new SpecReporter());
-  },
-
-  params: {
-    email: 'robin@a.com'
-  }
-
-  };
+};
